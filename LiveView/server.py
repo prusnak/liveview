6bf84252492ede198e5ef1500e0007c9a10da479
Pavol Rusnak <stick@gk2.sk>
2011-01-25 18:07:34 +0100

    start abstracting send method

--- LiveView/server.py
+++ LiveView/server.py
@@ -37,6 +37,9 @@ class Server:
         if self.__client:
             self.__client.close()
 
+    def send(self, msg):
+        self.__client.send(msg)
+
     def loop(self):
         while True:
             for msg in messages.Decode(self.__client.recv(4096)):
@@ -46,13 +49,13 @@ class Server:
                         print msg
                         continue
 
-                self.__client.send(messages.EncodeAck(msg.messageId))
+                self.send(messages.EncodeAck(msg.messageId))
 
                 if isinstance(msg, messages.GetMenuItems):
-                    self.__client.send(messages.EncodeGetMenuItemResponse(0, True, 0, "Menu0", self.testPng))
-                    self.__client.send(messages.EncodeGetMenuItemResponse(1, False, 20, "Menu1", self.testPng))
-                    self.__client.send(messages.EncodeGetMenuItemResponse(2, False, 0, "Menu2", self.testPng))
-                    self.__client.send(messages.EncodeGetMenuItemResponse(3, True, 0, "Menu3", self.testPng))
+                    self.send(messages.EncodeGetMenuItemResponse(0, True, 0, "Menu0", self.testPng))
+                    self.send(messages.EncodeGetMenuItemResponse(1, False, 20, "Menu1", self.testPng))
+                    self.send(messages.EncodeGetMenuItemResponse(2, False, 0, "Menu2", self.testPng))
+                    self.send(messages.EncodeGetMenuItemResponse(3, True, 0, "Menu3", self.testPng))
 
                 elif isinstance(msg, messages.GetMenuItem):
                     print "---- GetMenuItem received ----"
@@ -60,46 +63,46 @@ class Server:
 
                 elif isinstance(msg, messages.DisplayCapabilities):
                     deviceCapabilities = msg
-                    self.__client.send(messages.EncodeSetMenuSize(4))
-                    self.__client.send(messages.EncodeSetMenuSettings(self.menuVibrationTime, 0))
+                    self.send(messages.EncodeSetMenuSize(4))
+                    self.send(messages.EncodeSetMenuSettings(self.menuVibrationTime, 0))
 
                 elif isinstance(msg, messages.GetTime):
-                    self.__client.send(messages.EncodeGetTimeResponse(time.time(), self.is24HourClock))
+                    self.send(messages.EncodeGetTimeResponse(time.time(), self.is24HourClock))
 
                 elif isinstance(msg, messages.DeviceStatus):
-                    self.__client.send(messages.EncodeDeviceStatusAck())
+                    self.send(messages.EncodeDeviceStatusAck())
 
                 elif isinstance(msg, messages.GetAlert):
-                    self.__client.send(messages.EncodeGetAlertResponse(20, 4, 15, "TIME", "HEADER", "01234567890123456789012345678901234567890123456789", self.testPng))
+                    self.send(messages.EncodeGetAlertResponse(20, 4, 15, "TIME", "HEADER", "01234567890123456789012345678901234567890123456789", self.testPng))
 
                 elif isinstance(msg, messages.Navigation):
-                    self.__client.send(messages.EncodeNavigationResponse(messages.RESULT_EXIT))
+                    self.send(messages.EncodeNavigationResponse(messages.RESULT_EXIT))
 
-#                    self.__client.send(messages.EncodeSetMenuSize(0))
-#                    self.__client.send(messages.EncodeClearDisplay())
-#                    self.__client.send(messages.EncodeDisplayBitmap(100, 100, self.testPng))
-#                    self.__client.send(messages.EncodeSetScreenMode(50, False))
-#                    self.__client.send(messages.EncodeDisplayText("WOOOOOOOOOOOO"))
+#                    self.send(messages.EncodeSetMenuSize(0))
+#                    self.send(messages.EncodeClearDisplay())
+#                    self.send(messages.EncodeDisplayBitmap(100, 100, self.testPng))
+#                    self.send(messages.EncodeSetScreenMode(50, False))
+#                    self.send(messages.EncodeDisplayText("WOOOOOOOOOOOO"))
 
-#                    self.__client.send(messages.EncodeLVMessage(31, ""))
+#                    self.send(messages.EncodeLVMessage(31, ""))
 
-#                    self.__client.send(messages.EncodeSetScreenMode(0, False))
-#                    self.__client.send(messages.EncodeClearDisplay())
-#                    self.__client.send(messages.EncodeLVMessage(48, struct.pack(">B", 38) + "moo"))
+#                    self.send(messages.EncodeSetScreenMode(0, False))
+#                    self.send(messages.EncodeClearDisplay())
+#                    self.send(messages.EncodeLVMessage(48, struct.pack(">B", 38) + "moo"))
 
 #                    tmpxxx = "MOOO"
-#                    self.__client.send(messages.EncodeSetMenuSize(4))
-#                    self.__client.send(messages.EncodeDisplayText("moo"))
+#                    self.send(messages.EncodeSetMenuSize(4))
+#                    self.send(messages.EncodeDisplayText("moo"))
 
-#                    self.__client.send(messages.EncodeSetStatusBar(tmp.menuItemId, 200, self.testPng))
-#                    self.__client.send(EncodeLVMessage(5, messages.EncodeUIPayload(isAlertItem, totalAlerts, unreadAlerts, curAlert, menuItemId, top, mid, body, itemBitmap)))
+#                    self.send(messages.EncodeSetStatusBar(tmp.menuItemId, 200, self.testPng))
+#                    self.send(EncodeLVMessage(5, messages.EncodeUIPayload(isAlertItem, totalAlerts, unreadAlerts, curAlert, menuItemId, top, mid, body, itemBitmap)))
 
                     if msg.navType == messages.NAVTYPE_DOWN:
                         if not msg.wasInAlert:
-                            self.__client.send(messages.EncodeDisplayPanel("TOOOOOOOOOOOOOOOOOP", "BOTTTTTTTTTTTTTTTTTOM", self.testPng, False))
-#                        self.__client.send(messages.EncodeNavigationAck(messages.RESULT_OK))
-#                        self.__client.send(messages.EncodeDisplayText("ADQ WOS HERE"))
+                            self.send(messages.EncodeDisplayPanel("TOOOOOOOOOOOOOOOOOP", "BOTTTTTTTTTTTTTTTTTOM", self.testPng, False))
+#                        self.send(messages.EncodeNavigationAck(messages.RESULT_OK))
+#                        self.send(messages.EncodeDisplayText("ADQ WOS HERE"))
 #                    elif tmp.navType == messages.NAVTYPE_SELECT:
-#                        self.__client.send(messages.EncodeNavigationAck(messages.RESULT_EXIT))
-#                    self.__client.send(messages.EncodeSetVibrate(1, 1000))
+#                        self.send(messages.EncodeNavigationAck(messages.RESULT_EXIT))
+#                    self.send(messages.EncodeSetVibrate(1, 1000))
                 print msg
